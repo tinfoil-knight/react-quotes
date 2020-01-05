7055fa550cdebe1d57b9609012b9d97f72077d31
tinfoil-knight <kunal99kundu@gmail.com>
2020-01-05 12:28:38 +0530

    Updates

---
+++ precache-manifest.7fe9ea08a4ca7cfc083cd3452673d5fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f0563b03f9fc99f24398498b2c1091",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e7657cc26bc28ef4434",
+    "url": "./static/css/main.f86a9767.chunk.css"
+  },
+  {
+    "revision": "0d9e6cb89a0986e1c7a4",
+    "url": "./static/js/2.c5d32ee8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.c5d32ee8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e7657cc26bc28ef4434",
+    "url": "./static/js/main.cddd2118.chunk.js"
+  },
+  {
+    "revision": "7897109d03b4f87f430d",
+    "url": "./static/js/runtime-main.b4d61910.js"
+  },
+  {
+    "revision": "da77886b3aaed7460efacb2907bec278",
+    "url": "./static/media/fanwood-webfont.da77886b.woff"
+  }
+]);
